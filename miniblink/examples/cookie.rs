1fe1d3431a829e3c9979e1b6e2c209b621e0c656
felixmaker <7445790+felixmaker@users.noreply.github.com>
2024-03-26 10:45:24 +0800

    example: cookie

---
+++ miniblink/examples/cookie.rs
@@ -0,0 +1,26 @@
+use miniblink::{app::AppBuilder, webview::WebViewBuilder};
+
+fn main() {
+    let app = AppBuilder::default()
+        .build()
+        .expect("Failed to initialize miniblink!");
+
+    let _ = WebViewBuilder::default()
+        .with_window_title("Hello, Miniblink")
+        .with_url("https://u.4399.com/login.html")
+        .with_visible(true)
+        .with_on_window_closing_handler(|_| {
+            std::process::exit(0);
+        })
+        .with_on_document_ready_handler(|wv| {
+            let cookie = wv.get_cookie();
+            if cookie.contains("Uauth=") {
+                println!("User logined!");
+                println!("{}", cookie);
+                // do something...
+            }
+        })
+        .build();
+
+    app.run_message_loop();
+}
