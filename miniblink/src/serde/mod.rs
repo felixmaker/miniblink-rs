bc43812648292e91fa427cb7ba9081379a2974f4
felixmaker <7445790+felixmaker@users.noreply.github.com>
2024-03-29 21:41:03 +0800

    doc: fix serde

--- miniblink/src/serde/mod.rs
+++ miniblink/src/serde/mod.rs
@@ -1,6 +1,6 @@
-/// Deserializer for [`crate::value::JsValue`]
+/// Deserializer for [`crate::types::JsValue`]
 pub mod de;
-/// Serializer for [`crate::value::JsValue`]
+/// Serializer for [`crate::types::JsValue`]
 pub mod ser;
 
 pub use de::from_value;
