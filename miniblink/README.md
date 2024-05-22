d23356c0cc6560da1c70de6cc375eb6397fd85ef
felixmaker <7445790+felixmaker@users.noreply.github.com>
2024-05-22 21:20:44 +0800

    Update example hello

--- miniblink/README.md
+++ miniblink/README.md
@@ -13,7 +13,6 @@ use miniblink::{app, webview::*};
 fn main() {
     app::init("mb.dll").unwrap();
     let view = WebView::default();
-    view.move_to_center();
     view.load_url("https://example.com");
     view.on_close(|_| std::process::exit(0));
     view.show();
--- miniblink/examples/hello.rs
+++ miniblink/examples/hello.rs
@@ -1,9 +1,8 @@
 use miniblink::{app, webview::*};
 
 fn main() {
-    app::init("mb.dll").unwrap();
+    app::init("./mb.dll").unwrap();
     let view = WebView::default();
-    view.move_to_center();
     view.load_url("https://example.com");
     view.on_close(|_| std::process::exit(0));
     view.show();
